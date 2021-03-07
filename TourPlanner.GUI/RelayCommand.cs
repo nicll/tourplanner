5115ef0dd7545c67f13ae2d0148bd8f225d5b811
Nicolas Klement <nicolas.klement@gmail.com>
2021-03-08 00:03:22 +0100

    Add two RelayCommand implementations

---
+++ TourPlanner.GUI/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TourPlanner.GUI
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            _execute = methodToExecute;
+            _canExecute = canExecuteEvaluator;
+        }
+
+        public RelayCommand(Action methodToExecute)
+            : this(methodToExecute, null)
+        {
+        }
+
+        public bool CanExecute(object parameter)
+            => _canExecute == null || _canExecute.Invoke();
+
+        public void Execute(object parameter) => _execute.Invoke();
+    }
+}
