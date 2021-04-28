b4d552ba54cc013894e34aa558363aaacb779106
Nicolas Klement <nicolas.klement@gmail.com>
2021-04-28 20:00:04 +0200

    Implement helper classes for MapQuest data provider

---
+++ TourPlanner.DataProviders.MapQuest/RouteConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using TourPlanner.Core.Models;
+using TourPlanner.DataProviders.MapQuest.Models;
+using CoreRoute = TourPlanner.Core.Models.Route;
+
+namespace TourPlanner.DataProviders.MapQuest
+{
+    internal class RouteConverter : JsonConverter<CoreRoute>
+    {
+        public override CoreRoute Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var wrapper = JsonSerializer.Deserialize<RouteWrapper>(ref reader, options);
+            ref readonly var internalRoute = ref wrapper.Route;
+            return new CoreRoute { RouteId = internalRoute.SessionId, TotalDistance = internalRoute.Distance, Steps = ManeuversToSteps(internalRoute.Legs[0].Maneuvers) };
+        }
+
+        private static IReadOnlyList<Step> ManeuversToSteps(IList<Maneuver> maneuvers)
+            => maneuvers.Select(m => new Step { Distance = m.Distance, Description = m.Narrative }).ToList().AsReadOnly();
+
+        public override void Write(Utf8JsonWriter writer, CoreRoute value, JsonSerializerOptions options)
+        {
+            var wrapper = new RouteWrapper(new(value.RouteId, value.TotalDistance, new[] { new Leg(StepsToManeuvers(value.Steps)) }));
+            JsonSerializer.Serialize(writer, wrapper, options);
+        }
+
+        private static List<Maneuver> StepsToManeuvers(IReadOnlyList<Step> steps)
+            => steps.Select(s => new Maneuver(s.Distance, s.Description)).ToList();
+    }
+}
---
+++ TourPlanner.DataProviders.MapQuest/StringValueTupleCurrentCultureCaseInsensitiveComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TourPlanner.DataProviders.MapQuest
+{
+    internal class StringValueTupleCurrentCultureCaseInsensitiveComparer : IEqualityComparer<(string, string)>
+    {
+        public bool Equals((string, string) x, (string, string) y)
+            => StringComparer.CurrentCultureIgnoreCase.Equals(x.Item1, y.Item1)
+                && StringComparer.CurrentCultureIgnoreCase.Equals(x.Item2, y.Item2);
+
+        public int GetHashCode([DisallowNull] (string, string) obj)
+            => HashCode.Combine(obj);
+    }
+}
