46f6b6e8d58651971468d73ad60ebab53beb1fbe
Nicolas Klement <nicolas.klement@gmail.com>
2021-04-28 19:54:37 +0200

    Create empty PostgreSQL DB client

---
+++ TourPlanner.DB.Postgres/PostgresDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TourPlanner.Core.Interfaces;
+using TourPlanner.Core.Models;
+
+namespace TourPlanner.DB.Postgres
+{
+    public class PostgresDatabase : IDatabaseClient
+    {
+        public Task AddTour(Tour tour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Tour>> QueryTours()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveTour(Tour tour)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
