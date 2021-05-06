f51cd289303a2e0012bb27f29e95fbc5863ff345
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-07 01:03:36 +0200

    Update MainWindow view

---
+++ TourPlanner.GUI/Views/SingleTourView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace TourPlanner.GUI.Views
+{
+    /// <summary>
+    /// Interaktionslogik für SingleTourView.xaml
+    /// </summary>
+    public partial class SingleTourView : UserControl
+    {
+        public SingleTourView()
+        {
+            InitializeComponent();
+        }
+    }
+}
