447016063f457f4acd95cf3a46ac9951f3cf2513
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-07 01:06:10 +0200

    Include resources for AdonisUI

--- TourPlanner.GUI/App.xaml.cs
+++ TourPlanner.GUI/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace TourPlanner.GUI
 {
@@ -13,5 +10,10 @@ namespace TourPlanner.GUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
     }
 }
