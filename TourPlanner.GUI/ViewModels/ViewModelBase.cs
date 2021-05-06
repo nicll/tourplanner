85437de56630448583a4db7b669e5bd9530e11c1
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-07 01:04:28 +0200

    Add ViewModelBase

---
+++ TourPlanner.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TourPlanner.GUI.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property used to notify listeners.
+        /// May be automatically filled in.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Checks if a property already matches a desired value.
+        /// Sets the property and notifies listeners only when necessary.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="storage">Reference to the property with both getter and setter.</param>
+        /// <param name="value">Desired value for the property.</param>
+        /// <param name="propertyName">Name of the property used to notify listeners.
+        /// May be automatically filled in.</param>
+        /// <returns>Whether or not the value was changed.</returns>
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            // no change if same value
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            // update and notify
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+    }
+}
