a697e58fecc16e94a72dc235e7025567f28b748c
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-07 01:03:52 +0200

    Add AddTour view

---
+++ TourPlanner.GUI/Views/AddTourView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AdonisUI.Controls;
+using System;
+
+namespace TourPlanner.GUI.Views
+{
+    /// <summary>
+    /// Interaktionslogik für AddTourView.xaml
+    /// </summary>
+    public partial class AddTourView : AdonisWindow
+    {
+        public AddTourView()
+        {
+            InitializeComponent();
+        }
+    }
+}
