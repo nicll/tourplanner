dc56a77a159cef468dcf76ae2684e343d61222b7
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-08 00:47:45 +0200

    Add unit test project

---
+++ TourPlanner.Tests/MapQuestTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TourPlanner.Tests
+{
+    public class MapQuestTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TourPlanner.Tests/PdfTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System;
+
+namespace TourPlanner.Tests
+{
+    public class PdfTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
