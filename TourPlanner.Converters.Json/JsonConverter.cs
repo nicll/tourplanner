16b7fb5227790e7b16421cb389243ccba13fc2cb
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-08 01:45:01 +0200

    Add importer and exporter for json files

---
+++ TourPlanner.Converters.Json/JsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TourPlanner.Core.Interfaces;
+using TourPlanner.Core.Models;
+
+namespace TourPlanner.Converters.Json
+{
+    public class JsonConverter : IDataConverter
+    {
+        private static readonly JsonSerializerOptions _jsonOpts = new();
+
+        public string PreferredFileExtension => "json";
+
+        public async Task<ICollection<Tour>> ReadTours(Stream inputStream)
+            => await JsonSerializer.DeserializeAsync<ICollection<Tour>>(inputStream, _jsonOpts).ConfigureAwait(false);
+
+        public async Task WriteTours(Stream outputStream, ICollection<Tour> tours)
+            => await JsonSerializer.SerializeAsync(outputStream, tours, _jsonOpts).ConfigureAwait(false);
+    }
+}
---
+++ TourPlanner.Core/Interfaces/IDataConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using TourPlanner.Core.Models;
+
+namespace TourPlanner.Core.Interfaces
+{
+    public interface IDataConverter
+    {
+        /// <summary>
+        /// The preferred extension of the file when saved to disk.
+        /// </summary>
+        string PreferredFileExtension { get; }
+
+        /// <summary>
+        /// Read all tours from <paramref name="inputStream"/>.
+        /// </summary>
+        /// <param name="inputStream">The input data stream.</param>
+        /// <returns>All tours that could be read.</returns>
+        Task<ICollection<Tour>> ReadTours(Stream inputStream);
+
+        /// <summary>
+        /// Write all given tours into <paramref name="outputStream"/>.
+        /// </summary>
+        /// <param name="outputStream">The output data stream.</param>
+        /// <param name="tours">The given tours.</param>
+        Task WriteTours(Stream outputStream, ICollection<Tour> tours);
+    }
+}
