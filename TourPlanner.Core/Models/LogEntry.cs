ca89dea9dceb70e6b62b93afed727601b403a454
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-18 01:59:21 +0200

    Modify Tour and LogEntry models to make use of change tracking mechanisms

--- TourPlanner.Core/Models/LogEntry.cs
+++ TourPlanner.Core/Models/LogEntry.cs
@@ -1,30 +1,58 @@
 ﻿using System;
+using System.ComponentModel;
+using TourPlanner.Core.Internal;
 
 namespace TourPlanner.Core.Models
 {
     /// <summary>
     /// A log entry describes the times when a specific tour was performed.
     /// </summary>
-    public class LogEntry
+    public class LogEntry : IChangeTracking
     {
+        private readonly ChangeTracker _tracker = new();
+        private DateTime _date;
+        private double _distance;
+        private TimeSpan _duration;
+        private float _rating;
+
         /// <summary>
         /// Date and time when the log entry was performed.
         /// </summary>
-        public DateTime Date { get; set; }
+        public DateTime Date
+        {
+            get => _date;
+            set => _tracker.SetProperty(ref _date, value);
+        }
 
         /// <summary>
         /// The travelled distance.
         /// </summary>
-        public double Distance { get; set; }
+        public double Distance
+        {
+            get => _distance;
+            set => _tracker.SetProperty(ref _distance, value);
+        }
 
         /// <summary>
         /// The time spent travelling.
         /// </summary>
-        public TimeSpan Duration { get; set; }
+        public TimeSpan Duration
+        {
+            get => _duration;
+            set => _tracker.SetProperty(ref _duration, value);
+        }
 
         /// <summary>
         /// The user's rating of the tour.
         /// </summary>
-        public float Rating { get; set; }
+        public float Rating
+        {
+            get => _rating;
+            set => _tracker.SetProperty(ref _rating, value);
+        }
+
+        public bool IsChanged => _tracker.IsChanged;
+
+        public void AcceptChanges() => _tracker.AcceptChanges();
     }
 }
--- TourPlanner.Core/Models/Tour.cs
+++ TourPlanner.Core/Models/Tour.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using TourPlanner.Core.Internal;
 
 namespace TourPlanner.Core.Models
 {
     /// <summary>
     /// Contains all relevant information about a tour.
     /// </summary>
-    public class Tour
+    public class Tour : IChangeTracking
     {
+        private readonly ChangeTracker _tracker = new();
+        private readonly ChangeTrackingCollection<LogEntry> _log;
+        private string _name, _description;
+
         /// <summary>
         /// A unique ID for identifying the tour.
         /// </summary>
@@ -16,7 +22,11 @@ namespace TourPlanner.Core.Models
         /// <summary>
         /// A user-decided name for the tour.
         /// </summary>
-        public string Name { get; set; }
+        public string Name
+        {
+            get => _name;
+            set => _tracker.SetProperty(ref _name, value);
+        }
 
         /// <summary>
         /// File path to the preview image.
@@ -26,7 +36,11 @@ namespace TourPlanner.Core.Models
         /// <summary>
         /// A user-decided description for the tour.
         /// </summary>
-        public string CustomDescription { get; set; }
+        public string CustomDescription
+        {
+            get => _description;
+            set => _tracker.SetProperty(ref _description, value);
+        }
 
         /// <summary>
         /// Reference to a <see cref="Models.Route"/> object containting
@@ -37,6 +51,20 @@ namespace TourPlanner.Core.Models
         /// <summary>
         /// Contains the log entries for this tour.
         /// </summary>
-        public ICollection<LogEntry> Log { get; init; }
+        public ICollection<LogEntry> Log => _log;
+
+        public bool IsChanged => _tracker.IsChanged;
+
+        public void AcceptChanges()
+        {
+            _tracker.AcceptChanges();
+            _log.AcceptChanges();
+        }
+
+        public Tour()
+            => _log = new ChangeTrackingCollection<LogEntry>();
+
+        public Tour(ICollection<LogEntry> logEntries)
+            => _log = new ChangeTrackingCollection<LogEntry>(logEntries);
     }
 }
