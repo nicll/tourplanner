78aafc5edf1d1cc921db8a261b2e2cb78fb18f28
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-18 01:58:45 +0200

    Add internal change tracking utility classes to core

---
+++ TourPlanner.Core/Internal/ChangeTracker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace TourPlanner.Core.Internal
+{
+    internal class ChangeTracker : IChangeTracking
+    {
+        private bool _changed = false;
+
+        public bool IsChanged => _changed;
+
+        public void AcceptChanges()
+            => _changed = false;
+
+        /// <summary>
+        /// Checks if a property already matches a desired value.
+        /// Sets the property and notifies listeners only when necessary.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="storage">Reference to the property with both getter and setter.</param>
+        /// <param name="value">Desired value for the property.</param>
+        /// <returns>Whether or not the value was changed.</returns>
+        public bool SetProperty<T>(ref T storage, T value)
+        {
+            // no change if same value
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            // update and note
+            storage = value;
+            _changed = true;
+
+            return true;
+        }
+    }
+}
---
+++ TourPlanner.Core/Internal/ChangeTrackingCollection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace TourPlanner.Core.Internal
+{
+    /// <summary>
+    /// A collection that automatically keeps track of changed items.
+    /// It is assumed that every object is unique.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    internal class ChangeTrackingCollection<T> : ICollection<T>, IChangeTracking where T : IChangeTracking
+    {
+        private readonly List<T>
+            _currentItems = new(),
+            _newItems = new(),
+            _removedItems = new();
+
+        /// <summary>
+        /// A collection of all added items.
+        /// </summary>
+        public IReadOnlyCollection<T> NewItems => _newItems;
+
+        /// <summary>
+        /// A collection of all removed items.
+        /// </summary>
+        public IReadOnlyCollection<T> RemovedItems => _removedItems;
+
+        /// <summary>
+        /// A collection of all modified items.
+        /// </summary>
+        public IReadOnlyCollection<T> ChangedItems => this.Where(i => i.IsChanged).ToArray();
+
+        public int Count => _currentItems.Count + _newItems.Count - _removedItems.Count;
+
+        public bool IsReadOnly => false;
+
+        public bool IsChanged => _newItems.Any() || _removedItems.Any() || this.Any(i => i.IsChanged);
+
+        public ChangeTrackingCollection()
+        { }
+
+        public ChangeTrackingCollection(ICollection<T> initialItems)
+            => _currentItems.AddRange(initialItems);
+
+        public void Add(T item)
+        {
+            if (_newItems.Contains(item) || _currentItems.Contains(item))
+                throw new InvalidOperationException("");
+
+            _newItems.Add(item);
+        }
+
+        public void AddRange(ICollection<T> items)
+            => _newItems.AddRange(items);
+
+        public void Clear()
+        {
+            _newItems.Clear();
+            _removedItems.Clear();
+            _removedItems.AddRange(_currentItems);
+        }
+
+        public bool Contains(T item)
+            => _newItems.Contains(item) || (_currentItems.Contains(item) && !_removedItems.Contains(item));
+
+        public void CopyTo(T[] array, int arrayIndex)
+            => throw new NotSupportedException("This operation is not permitted.");
+
+        public IEnumerator<T> GetEnumerator()
+            => _currentItems.Except(_removedItems).Concat(_newItems).GetEnumerator();
+
+        public bool Remove(T item)
+        {
+            // not yet accepted, in new
+            if (_newItems.Remove(item))
+                return true;
+
+            // accepted
+            if (_currentItems.Contains(item) && !_removedItems.Contains(item))
+            {
+                _removedItems.Add(item);
+                return true;
+            }
+
+            // not yet accepted, already removed
+            return _removedItems.Contains(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        public void AcceptChanges()
+        {
+            foreach (var item in _removedItems)
+                _currentItems.Remove(item);
+
+            _currentItems.AddRange(_newItems);
+            _removedItems.Clear();
+            _newItems.Clear();
+
+            foreach (var item in _currentItems)
+                item.AcceptChanges();
+        }
+    }
+}
