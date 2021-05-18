be536c4c64ad3943a64c5548acc25637d241ae28
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-18 02:11:48 +0200

    Update DesignMainWindowViewModel to use new Tour ctor

--- TourPlanner.GUI/ViewModels/DesignMainWindowViewModel.cs
+++ TourPlanner.GUI/ViewModels/DesignMainWindowViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using TourPlanner.Core.Models;
 
 namespace TourPlanner.GUI.ViewModels
@@ -9,13 +8,12 @@ namespace TourPlanner.GUI.ViewModels
     {
         public DesignMainWindowViewModel()
         {
-            SelectedTour = new Tour
+            SelectedTour = new Tour(new List<LogEntry> { new() { Date = DateTime.Now, Distance = 1.23, Duration = TimeSpan.FromMinutes(30), Rating = 0.77F }, new() })
             {
                 Name = "Beispielstour",
                 CustomDescription = "Meine Beschreibung",
                 Route = new Route { Steps = new[] { new Step { Distance = 1.23, Description = "Schritt 1" }, new() { Distance = 2.34, Description = "Schritt 2" } } },
-                ImagePath = @"C:\Users\Nicolas\Programming\repos\tourplanner\TourPlanner.GUI\bin\Debug\net5.0-windows\TourPlanner_CachedImages\60888fee-0235-4ee4-02b4-3532-0e3fbcda8225.png",
-                Log = new List<LogEntry> { new() { Date = DateTime.Now, Distance = 1.23, Duration = TimeSpan.FromMinutes(30), Rating = 0.77F }, new() }
+                ImagePath = @"C:\Users\Nicolas\Programming\repos\tourplanner\TourPlanner.GUI\bin\Debug\net5.0-windows\TourPlanner_CachedImages\60888fee-0235-4ee4-02b4-3532-0e3fbcda8225.png"
             };
             ShownTours = new() { SelectedTour, SelectedTour, SelectedTour, SelectedTour, new Tour { Name = "Test 2", CustomDescription = "leer", Route = new() { TotalDistance = 0.1, Steps = new[] { new Step(), new Step() } } } };
             InitializationFinished += (_, _) => UpdateLoadedTours(ShownTours);
