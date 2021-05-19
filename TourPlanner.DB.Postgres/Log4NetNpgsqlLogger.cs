ce712157b01df58a212266c0128f28463105767e
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-19 15:42:39 +0200

    Add logging provider for Npgsql

---
+++ TourPlanner.DB.Postgres/Log4NetNpgsqlLogger.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using log4net.Core;
+using Npgsql.Logging;
+using System;
+
+namespace TourPlanner.DB.Postgres
+{
+    // from https://stackoverflow.com/a/43107110/13282284
+    public class Log4NetNpgsqlLogger : NpgsqlLogger
+    {
+        private readonly ILog _log;
+
+        public Log4NetNpgsqlLogger(string name)
+            => _log = LogManager.GetLogger(name);
+
+        public override bool IsEnabled(NpgsqlLogLevel level)
+            => _log.Logger.IsEnabledFor(GetLog4NetLevelFromNpgsqlLogLevel(level));
+
+        public override void Log(NpgsqlLogLevel level, int connectorId, string msg, Exception exception = null)
+            => _log.Logger.Log(typeof(NpgsqlLogger), GetLog4NetLevelFromNpgsqlLogLevel(level), connectorId + ": " + msg, exception);
+
+        protected Level GetLog4NetLevelFromNpgsqlLogLevel(NpgsqlLogLevel level) => level switch
+        {
+            NpgsqlLogLevel.Trace or NpgsqlLogLevel.Debug => Level.Debug,
+            NpgsqlLogLevel.Info => Level.Info,
+            NpgsqlLogLevel.Warn => Level.Warn,
+            NpgsqlLogLevel.Error => Level.Error,
+            NpgsqlLogLevel.Fatal => Level.Fatal,
+            _ => throw new Exception("Unknown Npgsql Log Level: " + level),
+        };
+    }
+}
---
+++ TourPlanner.DB.Postgres/Log4NetNpgsqlLoggingProvider.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql.Logging;
+using System;
+
+namespace TourPlanner.DB.Postgres
+{
+    // from https://stackoverflow.com/a/43107110/13282284
+    public class Log4NetNpgsqlLoggingProvider : INpgsqlLoggingProvider
+    {
+        public string DefaultLoggerName { get; }
+
+        public string CommandLoggerName { get; set; }
+
+        public Log4NetNpgsqlLoggingProvider(string defaultLoggerName)
+            => DefaultLoggerName = defaultLoggerName ?? throw new ArgumentNullException(nameof(defaultLoggerName));
+
+        public NpgsqlLogger CreateLogger(string name) => name switch
+        {
+            "Npgsql.NpgsqlCommand" => new Log4NetNpgsqlLogger(CommandLoggerName ?? DefaultLoggerName),
+            _ => new Log4NetNpgsqlLogger(DefaultLoggerName),
+        };
+    }
+}
