eb62163b6426bf894f631a23e4897d288fa78ddb
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-24 23:00:15 +0200

    Add column in DataGrid for icon and disable unusable buttons and text boxes

---
+++ TourPlanner.GUI/NotNullConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TourPlanner.GUI
+{
+    public class NotNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is not null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new InvalidOperationException("IsNullConverter can only be used OneWay.");
+    }
+}
---
+++ TourPlanner.GUI/RelativeToFullPathConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace TourPlanner.GUI
+{
+    public class RelativeToFullPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => Path.GetFullPath(value as string ?? throw new ArgumentException("Value is not a string.", nameof(value)));
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new InvalidOperationException("Cannot convert absolute path to relative path.");
+    }
+}
