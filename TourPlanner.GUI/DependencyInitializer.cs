ec220395a8c489f7e9c32b9fc655a67a806d23f5
Nicolas Klement <nicolas.klement@gmail.com>
2021-06-07 17:48:01 +0200

    Add some more documentation to DependencyInitializer

--- TourPlanner.GUI/DependencyInitializer.cs
+++ TourPlanner.GUI/DependencyInitializer.cs
@@ -10,12 +10,23 @@ using TourPlanner.Reporting.PDF;
 
 namespace TourPlanner.GUI
 {
+    /// <summary>
+    /// Helper class for initializing dependencies.
+    /// </summary>
     internal static class DependencyInitializer
     {
+        /// <summary>
+        /// Initializes a <see cref="IDataManager"/> for a production environment.
+        /// </summary>
+        /// <returns>An initialized <see cref="IDataManager"/>.</returns>
         public static async Task<IDataManager> InitializeRealDataManager()
             => await DataManager.CreateDataManager(OSInteraction.LoadConfig("connection.config"),
                 new MapQuestApiFactory(), new PostgresDatabaseFactory(), new ReportGenerator(), new JsonConverter());
 
+        /// <summary>
+        /// Initializes a <see cref="IDataManager"/> for a testing environment.
+        /// </summary>
+        /// <returns>An initialized <see cref="IDataManager"/>.</returns>
         public static async Task<IDataManager> InitializeDummyDataManager()
             => await DataManager.CreateDataManager(OSInteraction.LoadConfig("connection.config"),
                 new MapQuestApiFactory(), new InMemoryDbFactory(), new ReportGenerator(), new JsonConverter());
